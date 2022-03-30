f794cbcfeb41bb0ceaad1c01a19b3af37aece49a
leovs09 <leovs010@gmail.com>
2022-03-30 23:30:36 +0200

    add base test cases for deposit

--- test/ERC20/index.ts
+++ test/ERC20/index.ts
@@ -1,10 +1,20 @@
-import * as ERC20ABI from "./ERC20.json";
 import { ethers } from "hardhat";
 import { IERC20 } from "../../typechain";
+// TODO: use this plugin instaed save API https://www.npmjs.com/package/hardhat-etherscan-abi
+import { WETHABI } from "./WETH";
+import { PayableOverrides, ContractTransaction } from "ethers";
+
+export interface IWETH extends IERC20 {
+  deposit(
+    overrides?: PayableOverrides & { from?: string | Promise<string> }
+  ): Promise<ContractTransaction>;
+}
 
 // based on https://stackoverflow.com/questions/71106843/check-balance-of-erc20-token-in-hardhat-using-ethers-js
-export const getERC20Contract = (address: string): IERC20 => {
-  const Contract = new ethers.Contract(address, ERC20ABI, ethers.provider);
+// https://etherscan.io/address/0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2#code
 
-  return Contract as IERC20;
+export const getWETHContract = (address: string): IWETH => {
+  const Contract = new ethers.Contract(address, WETHABI, ethers.provider);
+
+  return Contract as IWETH;
 };
--- test/EquityFund.test.ts
+++ test/EquityFund.test.ts
@@ -1,28 +1,22 @@
 import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
 import { expect } from "chai";
-import { BigNumberish, ContractTransaction, PayableOverrides } from "ethers";
+import { BigNumberish } from "ethers";
 import { ethers, upgrades } from "hardhat";
 // eslint-disable-next-line node/no-missing-import
-import { EquityFund, IERC20 } from "../typechain";
-import { getERC20Contract } from "./ERC20";
+import { EquityFund } from "../typechain";
+import { getWETHContract, IWETH } from "./ERC20";
 
 // WETH address in etherium mainnet
 const WETH_ADDRESS = "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2";
 
 const { formatEther: fromEth, parseEther: toEth } = ethers.utils;
 
-interface IWETH extends IERC20 {
-  deposit(
-    overrides?: PayableOverrides & { from?: string | Promise<string> }
-  ): Promise<ContractTransaction>;
-}
-
 const expectEth = (wei: BigNumberish) => expect(fromEth(wei));
 
 describe("EquityFund", function () {
   let contract: EquityFund;
-  let owner: SignerWithAddress;
-  let ownerAddres: string;
+  let owners: Array<SignerWithAddress>;
+  let ownerAddresses: Array<string>;
   let WETH: IWETH;
 
   before(async () => {
@@ -35,22 +29,116 @@ describe("EquityFund", function () {
       EquityFundFactory
     )) as EquityFund;
 
-    [owner] = await ethers.getSigners();
-    ownerAddres = await owner.getAddress();
+    owners = await ethers.getSigners();
+    ownerAddresses = await Promise.all(
+      owners.map(async (owner) => {
+        return await owner.getAddress();
+      })
+    );
 
-    WETH = getERC20Contract(WETH_ADDRESS) as IWETH;
-
-    const tx = await WETH.deposit({ value: toEth("10") });
-    await tx.wait();
+    WETH = getWETHContract(WETH_ADDRESS);
   });
 
   it("Should deposit tokens", async function () {
-    const approveTx = await WETH.approve(contract.address, toEth("10"));
-    await approveTx.wait();
+    const ownerAddres = ownerAddresses[0];
+    await depositAndApproveEth2Weth(
+      WETH,
+      owners[0],
+      toEth("10"),
+      contract.address
+    );
 
-    const tx = await contract.deposit(toEth("10"), ownerAddres, ownerAddres);
-    await tx.wait();
+    await contract.deposit(toEth("10"), ownerAddres, ownerAddres);
 
+    expectEth(await WETH.balanceOf(contract.address)).to.equal("10.0");
+    expectEth(await contract.totalSupply()).to.equal("10.0");
     expectEth(await contract.balanceOf(ownerAddres)).to.equal("10.0");
   });
+
+  it("Should deposit second owner tokens", async function () {
+    const ownerAddres = ownerAddresses[1];
+    await depositAndApproveEth2Weth(
+      WETH,
+      owners[1],
+      toEth("5"),
+      contract.address
+    );
+
+    await contract.deposit(toEth("5"), ownerAddres, ownerAddres);
+
+    expectEth(await WETH.balanceOf(contract.address)).to.equal("15.0");
+    expectEth(await contract.totalSupply()).to.equal("15.0");
+    expectEth(await contract.balanceOf(ownerAddres)).to.equal("5.0");
+  });
+
+  it("Should issue shares proportianaly to current assets (increase)", async function () {
+    // increase balance of vault without issue shares
+    await transferEthInWeth(WETH, owners[0], toEth("5"), contract.address);
+    // deposit from new address
+    const ownerAddres = ownerAddresses[2];
+    await depositAndApproveEth2Weth(
+      WETH,
+      owners[2],
+      toEth("10"),
+      contract.address
+    );
+
+    await contract.deposit(toEth("10"), ownerAddres, ownerAddres);
+
+    expectEth(await WETH.balanceOf(contract.address)).to.equal("30.0");
+    expectEth(await contract.totalSupply()).to.equal("22.5");
+    expectEth(await contract.balanceOf(ownerAddres)).to.equal("7.5");
+  });
+
+  it("Should issue shares proportianaly to current assets (decrease)", async function () {
+    // decrease balance of vault without changes shares
+    await WETH.connect(contract.signer).transfer(
+      ownerAddresses[0],
+      toEth("15")
+    );
+    // deposit from new address
+    const ownerAddres = ownerAddresses[2];
+    await depositAndApproveEth2Weth(
+      WETH,
+      owners[2],
+      toEth("10"),
+      contract.address
+    );
+
+    await contract.deposit(toEth("10"), ownerAddres, ownerAddres);
+
+    expectEth(await WETH.balanceOf(contract.address)).to.equal("15.0");
+    expectEth(await contract.totalSupply()).to.equal("37.5");
+    expectEth(await contract.balanceOf(ownerAddres)).to.equal("15.0");
+  });
 });
+
+async function depositAndApproveEth2Weth(
+  WETH: IWETH,
+  signer: SignerWithAddress,
+  value: BigNumberish,
+  spender: string
+) {
+  WETH = WETH.connect(signer);
+
+  const sendTx = await WETH.deposit({ value });
+  await sendTx.wait();
+
+  const approveTx = await WETH.approve(spender, value);
+  await approveTx.wait();
+}
+
+async function transferEthInWeth(
+  WETH: IWETH,
+  signer: SignerWithAddress,
+  value: BigNumberish,
+  to: string
+) {
+  WETH = WETH.connect(signer);
+
+  const sendTx = await WETH.deposit({ value });
+  await sendTx.wait();
+
+  const approveTx = await WETH.transfer(to, value);
+  await approveTx.wait();
+}
