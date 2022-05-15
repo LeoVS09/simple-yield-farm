765f11f4a60550943877910c5aec8afcfa71800b
leovs09 <leovs010@gmail.com>
2022-05-16 00:24:51 +0200

    increase usdt balance for default signer

--- deploy/010_deploy_test_USDT.ts
+++ deploy/010_deploy_test_USDT.ts
@@ -5,6 +5,7 @@ import { DeployFunction } from "hardhat-deploy/types";
 import { parseEther } from "ethers/lib/utils";
 import { ethers } from "hardhat";
 import { USDT } from "../typechain";
+import UsdtAbi from "../local/USDT_abi.json";
 
 // Binance Smart Chain
 // const iETH_address = "0xd57E1425837567F74A35d07669B23Bfb67aA4A93";
@@ -13,6 +14,7 @@ import { USDT } from "../typechain";
 // const iETH_address = "0x5ACD75f21659a59fFaB9AEBAf350351a8bfaAbc0";
 const iUSDT_address = "0x1180c114f7fAdCB6957670432a3Cf8Ef08Ab5354";
 const USDT_address = "0xdAC17F958D2ee523a2206206994597C13D831ec7";
+const USDT_owner_address = "0xc6cde7c39eb2f0f0095f41570af89efc2c1ea828";
 
 const NULL_ADDDRESS = "0x0000000000000000000000000000000000000000";
 
@@ -20,7 +22,7 @@ const isLocal = process.env.NODE_ENV === "local";
 console.log("isLocal", isLocal);
 
 const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
-  const { deployments, getNamedAccounts } = hre;
+  const { deployments, getNamedAccounts, network } = hre;
   const { deploy } = deployments;
 
   const { deployer } = await getNamedAccounts();
@@ -28,26 +30,22 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   console.log("deployer", deployer);
 
   if (isLocal) {
-    const deployment = await deploy("USDT", {
-      from: deployer,
-      // Lender unknown during deployment, will start with null address
-      log: true,
-      autoMine: true, // speed up deployment on local network (ganache, hardhat), no effect on live networks,
-      //   to: USDT_address,
+    await hre.network.provider.request({
+      method: "hardhat_impersonateAccount",
+      params: [USDT_owner_address],
     });
-
-    // Write this in `npx hardhat console` to give some tokens to account
-    const Usdt = (await ethers.getContractAt(
-      "USDT",
-      deployment.address
-    )) as USDT;
-
-    const address_for_balance = deployer;
-    await Usdt.mint(address_for_balance, parseEther("1000.0"));
+    const signer = await ethers.getSigner(USDT_owner_address);
+    const usdtContract = new ethers.Contract(USDT_address, UsdtAbi, signer);
+    await usdtContract.issue(ethers.utils.parseEther("10000"));
     console.log(
-      "USDT balance",
-      address_for_balance,
-      await Usdt.balanceOf(address_for_balance)
+      "owner usdt balance",
+      ethers.utils.formatEther(await usdtContract.balanceOf(USDT_owner_address))
+    );
+
+    await usdtContract.transfer(deployer, ethers.utils.parseEther("10000"));
+    console.log(
+      "deployer usdt balance",
+      ethers.utils.formatEther(await usdtContract.balanceOf(deployer))
     );
   }
 };
